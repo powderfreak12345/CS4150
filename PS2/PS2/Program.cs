321bcf37770b5ee2b1771d6d8dac9cefc96ed1dd
jabeutler2195 <JABeutler2195@gmail.com>
2018-09-05 23:18:55 -0600

    First pass debugging.   Fix some errors where unique tree count is not reduced when duplicates are found, among other things

--- PS2/PS2/Program.cs
+++ PS2/PS2/Program.cs
@@ -8,8 +8,8 @@ namespace PS2
         static void Main(string[] args)
         {
             String line;
-            LinkedList<SimpleBinaryTree> uniqueTrees = new LinkedList<SimpleBinaryTree>();
-            LinkedList<SimpleBinaryTree> nonUniqueTrees = new LinkedList<SimpleBinaryTree>();
+            LinkedList<BinaryTreeShape> uniqueTrees = new LinkedList<BinaryTreeShape>();
+            LinkedList<BinaryTreeShape> nonUniqueTrees = new LinkedList<BinaryTreeShape>();
             int uniqueTreeCount = 0;
 
 
@@ -28,7 +28,7 @@ namespace PS2
                 line = Console.ReadLine();
                 
                 // Create a new SBT
-                SimpleBinaryTree currentTree = new SimpleBinaryTree();
+                BinaryTreeShape currentTree = new BinaryTreeShape();
 
                 // LOOP Iterate across the line, parsing numbers and adding them to the tree
                 string unparsedCRV;
@@ -51,7 +51,7 @@ namespace PS2
                 bool isUnique = true;
                 
                 // Iterate through all non-uunique trees, look for a match
-                foreach (SimpleBinaryTree tree in nonUniqueTrees)
+                foreach (BinaryTreeShape tree in nonUniqueTrees)
                 {
                     // If this tree matches a non-unique tree, add this tree to the non-unique tree list
                     if (currentTree.Equals(tree))
@@ -65,13 +65,16 @@ namespace PS2
                 if (isUnique)
                 {
                     // Iterate through all unique trees, look for a match
-                    foreach (SimpleBinaryTree tree in uniqueTrees)
+                    foreach (BinaryTreeShape tree in uniqueTrees)
                     {
                         // If this tree matches a unique tree, add this tree and the unique tree to the non-unique tree list// If this tree matches a non-unique tree, add this tree to the non-unique tree list
                         if (currentTree.Equals(tree))
                         {
                             nonUniqueTrees.AddLast(currentTree);
+                            // We don't need to add tree to non-Unique trees, since its shape is already represented by currentTree
+                            uniqueTrees.Remove(tree);
                             isUnique = false;
+                            uniqueTreeCount--;
                             break;
                         }
                     }
@@ -80,14 +83,102 @@ namespace PS2
                 // If reached, the tree is unique.  Increment the unique counter
                 if (isUnique)
                 {
+                    uniqueTrees.AddLast(currentTree);
                     uniqueTreeCount++;
                 }
 
                 
             }
 
-            Console.WriteLine(uniqueTreeCount);
+            int nonUniqueTreeShapeCount = nonUniqueTrees.Count;
 
+            Console.WriteLine(uniqueTreeCount + nonUniqueTreeShapeCount);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    class BinaryTreeShape
+    {
+        private Dictionary<int, int> nodes;
+        private HashSet<int> leafNodes;
+        private int size;
+
+        /// <summary>
+        /// Creates a simple binary tree.
+        /// </summary>
+        public BinaryTreeShape()
+        {
+            nodes = new Dictionary<int, int>();
+            leafNodes = new HashSet<int>();
+        }
+
+        /// <summary>
+        /// Adds an integer to the tree.
+        /// </summary>
+        /// <param name="n"></param>
+        public void Add(int value)
+        {
+            int loc = 0;
+            bool spaceFound = false;
+
+            while (spaceFound == false)
+            {
+                int locValue;
+
+                if (!nodes.TryGetValue(loc, out locValue))
+                {
+                    nodes.Add(loc, value);  // If there is not already a node at this location, create one with this value
+                    leafNodes.Add(loc);     // Any new node is a leaf node, until a leaf node is added below it
+                    spaceFound = true;
+                }
+                else
+                {
+                    leafNodes.Remove(loc); // Remove loc from leafNodes, since it is no longer a leaf
+
+                    // Do a comparison, decide whethere to advance left or right from the current node
+                    if (value < locValue)
+                    {
+                        loc = (loc * 2) + 1;  // Make loc the left child of loc
+                    }
+                    else
+                    {
+                        loc = (loc * 2) + 2;  // Make loc the right child of loc
+                    }
+                }
+            }
+
+            size++;
+        }
+
+
+        /// <summary>
+        /// Compares two simple binary trees for tree shape.  Node values are not considered when comparing for tree shape.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public bool Equals(BinaryTreeShape t)
+        {
+            if (this.size != t.size)
+            {
+                throw new Exception("Trying to compare trees of different size.  Something is wrong");
+            }
+
+            foreach (int leafLocation in this.leafNodes)
+            {
+                if (!t.leafNodes.Contains(leafLocation))
+                {
+                    return false;   // If one SBT contains a leaf that another does not, they are not equal
+                }
+            }
+            return true;    // If reached, both contain all the same leafs
         }
     }
 }
