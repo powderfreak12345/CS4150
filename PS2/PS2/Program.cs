5c2175fc30e2a9d22f12f24e3667139b54524f36
jabeutler2195 <JABeutler2195@gmail.com>
2018-09-05 21:15:06 -0600

    Start PS2.  Copy n and k parsing from PS1

---
+++ PS2/PS2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PS2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+            int n = -1;
+            int k = -1;
+
+            // Throw away the parsing of k and n.  They do not contribute to the algorithm at all
+            line = Console.ReadLine();
+            int whitespaceIndex = line.IndexOf(" ");
+            string n_Unparsed = line.Substring(0, whitespaceIndex);
+            string k_Unparsed = line.Substring(whitespaceIndex + 1, line.Length - whitespaceIndex - 1);
+            n = int.Parse(n_Unparsed);
+            k = int.Parse(k_Unparsed);
+        }
+    }
+}
