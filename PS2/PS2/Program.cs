d9cd601176852e0ecdacbaeaa2e9cda1fb3dc075
jabeutler2195 <JABeutler2195@gmail.com>
2018-09-05 22:29:06 -0600

    Finish implementing main method.  Untested.

--- PS2/PS2/Program.cs
+++ PS2/PS2/Program.cs
@@ -10,6 +10,7 @@ namespace PS2
             String line;
             LinkedList<SimpleBinaryTree> uniqueTrees = new LinkedList<SimpleBinaryTree>();
             LinkedList<SimpleBinaryTree> nonUniqueTrees = new LinkedList<SimpleBinaryTree>();
+            int uniqueTreeCount = 0;
 
 
             // Parse n and k
@@ -21,26 +22,71 @@ namespace PS2
             int k = int.Parse(k_Unparsed);
 
             // LOOP Iterate by line for each new binary tree added.
+            for (int current = 1; current <= n; current++)
             {
-                /// Create a new SBT
+                // Get the next line so we can begin parsing it
+                line = Console.ReadLine();
                 
+                // Create a new SBT
+                SimpleBinaryTree currentTree = new SimpleBinaryTree();
+
                 // LOOP Iterate across the line, parsing numbers and adding them to the tree
+                string unparsedCRV;
+                int collapseResistanceValue;
+
+                // For layers 1 through k-1, parse CRVs and add them to tree
+                for (int layer = 1; layer < k; layer ++)
                 {
-
+                    whitespaceIndex = line.IndexOf(" ");        // Find the next space separating unparsed values
+                    unparsedCRV = line.Substring(0, whitespaceIndex);   // Get the unparsed integer from the line
+                    line = line.Substring(whitespaceIndex + 1, line.Length - whitespaceIndex - 1);  // Save the rest of the line for later extractions
+                    collapseResistanceValue = int.Parse(unparsedCRV);   // Parse the CRV into an int
+                    currentTree.Add(collapseResistanceValue);   // Add the CRV to the tree
                 }
+                // Special case: layer k.  Parse CRV and add to the tree
+                collapseResistanceValue = int.Parse(line);
+                currentTree.Add(collapseResistanceValue);
 
+                // Tells whether this tree is unique.  Innocent until proven guilty.
+                bool isUnique = true;
+                
                 // Iterate through all non-uunique trees, look for a match
+                foreach (SimpleBinaryTree tree in nonUniqueTrees)
                 {
                     // If this tree matches a non-unique tree, add this tree to the non-unique tree list
-                    //  Then break
+                    if (currentTree.Equals(tree))
+                    {
+                        nonUniqueTrees.AddLast(currentTree);
+                        isUnique = false;
+                        break;
+                    }
                 }
 
-                // Iterate through all unique trees, look for a match
+                if (isUnique)
                 {
-                    // If this tree matches a unique tree, add this tree and the unique tree to the non-unique tree list
-                        // Then break
+                    // Iterate through all unique trees, look for a match
+                    foreach (SimpleBinaryTree tree in uniqueTrees)
+                    {
+                        // If this tree matches a unique tree, add this tree and the unique tree to the non-unique tree list// If this tree matches a non-unique tree, add this tree to the non-unique tree list
+                        if (currentTree.Equals(tree))
+                        {
+                            nonUniqueTrees.AddLast(currentTree);
+                            isUnique = false;
+                            break;
+                        }
+                    }
                 }
+
+                // If reached, the tree is unique.  Increment the unique counter
+                if (isUnique)
+                {
+                    uniqueTreeCount++;
+                }
+
+                
             }
+
+            Console.WriteLine(uniqueTreeCount);
 
         }
     }
---
+++ PS2/PS2/SimpleBinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS2
+{
+    class SimpleBinaryTree
+    {
+        private Dictionary<int, int> nodes;
+        private HashSet<int> leafNodes;
+        private int size;
+        
+        /// <summary>
+        /// Creates a simple binary tree.
+        /// </summary>
+        public SimpleBinaryTree()
+        {
+            nodes = new Dictionary<int, int>();
+            leafNodes = new HashSet<int>();
+        }
+
+        /// <summary>
+        /// Adds an integer to the tree.
+        /// </summary>
+        /// <param name="n"></param>
+        public void Add(int value)
+        {
+            int loc = 0;
+            bool spaceFound = false;
+
+            while (spaceFound == false)
+            {
+                int locValue;
+
+                if (! nodes.TryGetValue(loc, out locValue))
+                {                    
+                    nodes.Add(loc, value);  // If there is not already a node at this location, create one with this value
+                    leafNodes.Add(loc);     // Any new node is a leaf node, until a leaf node is added below it
+                    spaceFound = true;
+                }
+                else
+                {                    
+                    leafNodes.Remove(loc); // Remove loc from leafNodes, since it is no longer a leaf
+
+                    // Do a comparison, decide whethere to advance left or right from the current node
+                    if (value < locValue)
+                    {                        
+                        loc = (loc * 2) + 1;  // Make loc the left child of loc
+                    }
+                    else
+                    {
+                        loc = (loc * 2) + 2;  // Make loc the right child of loc
+                    }
+                }                
+            }
+
+            size++;
+        }
+        
+
+        /// <summary>
+        /// Compares two simple binary trees for tree shape.  Node values are not considered when comparing for tree shape.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public bool Equals(SimpleBinaryTree t)
+        {
+            if (this.size != t.size)
+            {
+                throw new Exception("Trying to compare trees of different size.  Something is wrong");
+            }
+
+            foreach (int leafLocation in this.leafNodes)
+            {
+                if (! t.leafNodes.Contains(leafLocation))
+                {
+                    return false;   // If one SBT contains a leaf that another does not, they are not equal
+                }
+            }
+            return true;    // If reached, both contain all the same leafs
+        }
+
+
+
+
+
+
+    }
+}
