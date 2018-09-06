881387516b14fed89cd996391c3ec9c1a2623599
jabeutler2195 <JABeutler2195@gmail.com>
2018-09-05 22:44:24 -0600

    Combine everything into a single .cs file for easy submitting

--- PS2/PS2/SimpleBinaryTree.cs
+++ PS2/PS2/SimpleBinaryTree.cs
@@ -4,16 +4,16 @@ using System.Text;
 
 namespace PS2
 {
-    class SimpleBinaryTree
+    class BinaryTreeShape
     {
         private Dictionary<int, int> nodes;
         private HashSet<int> leafNodes;
         private int size;
-        
+
         /// <summary>
         /// Creates a simple binary tree.
         /// </summary>
-        public SimpleBinaryTree()
+        public BinaryTreeShape()
         {
             nodes = new Dictionary<int, int>();
             leafNodes = new HashSet<int>();
@@ -32,38 +32,38 @@ namespace PS2
             {
                 int locValue;
 
-                if (! nodes.TryGetValue(loc, out locValue))
-                {                    
+                if (!nodes.TryGetValue(loc, out locValue))
+                {
                     nodes.Add(loc, value);  // If there is not already a node at this location, create one with this value
                     leafNodes.Add(loc);     // Any new node is a leaf node, until a leaf node is added below it
                     spaceFound = true;
                 }
                 else
-                {                    
+                {
                     leafNodes.Remove(loc); // Remove loc from leafNodes, since it is no longer a leaf
 
                     // Do a comparison, decide whethere to advance left or right from the current node
                     if (value < locValue)
-                    {                        
+                    {
                         loc = (loc * 2) + 1;  // Make loc the left child of loc
                     }
                     else
                     {
                         loc = (loc * 2) + 2;  // Make loc the right child of loc
                     }
-                }                
+                }
             }
 
             size++;
         }
-        
+
 
         /// <summary>
         /// Compares two simple binary trees for tree shape.  Node values are not considered when comparing for tree shape.
         /// </summary>
         /// <param name="t"></param>
         /// <returns></returns>
-        public bool Equals(SimpleBinaryTree t)
+        public bool Equals(BinaryTreeShape t)
         {
             if (this.size != t.size)
             {
@@ -72,18 +72,12 @@ namespace PS2
 
             foreach (int leafLocation in this.leafNodes)
             {
-                if (! t.leafNodes.Contains(leafLocation))
+                if (!t.leafNodes.Contains(leafLocation))
                 {
                     return false;   // If one SBT contains a leaf that another does not, they are not equal
                 }
             }
             return true;    // If reached, both contain all the same leafs
         }
-
-
-
-
-
-
     }
 }
